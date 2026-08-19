f4ec34ed73b68dc430ed64c91758e5d50c66bfad
GUILHERME ORIGE KERNBICHLER <guilherme.kernbichler@senaisp.edu.br>
2026-08-19 19:18:17 -0300

    feat: cria a forma do tabuleiro

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 
 export default function Square() {
+  function Square() {
+    return <button className="square">1</button>
+  }
+
   return (
     <>
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+      <div>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+
+      <div>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      
+      <div>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
     </>
   );
 }
