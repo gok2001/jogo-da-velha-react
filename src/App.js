90bbb18a925c2f204640da0df63990281871c190
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2026-08-19 20:48:54 -0300

    feat: começa a estilizar o tabuleiro

--- src/App.js
+++ src/App.js
@@ -30,22 +30,22 @@ export default function Tabuleiro() {
 
   return (
     <>
-    <div>
-      <Square valor={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square valor={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square valor={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-
-    <div>
-      <Square valor={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square valor={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square valor={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-
-    <div>
-      <Square valor={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square valor={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square valor={squares[8]} onSquareClick={() => handleClick(8)} />
+    <div class="tabuleiro">
+      <div className="linha">
+        <Square valor={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square valor={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square valor={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="linha">
+        <Square valor={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square valor={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square valor={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="linha">
+        <Square valor={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square valor={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square valor={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </div>
     </>
   );
