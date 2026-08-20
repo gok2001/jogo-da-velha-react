4d5f4f7e8087b81e75f47606698f4e099d96d387
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2026-08-19 21:24:14 -0300

    feat: finaliza o jogo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function Square({valor, onSquareClick}) {
   );
 }
 
-export default function Tabuleiro({ xIsNext, squares, onPlay }) {
+function Tabuleiro({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculaVencedor(squares)) {
       return;
@@ -35,7 +35,6 @@ export default function Tabuleiro({ xIsNext, squares, onPlay }) {
 
   return (
     <>
-
     <div class="game-container">
       <h1 className="game-title">Jogo da Velha</h1>
       <div className="status">{status}</div>
@@ -59,6 +58,50 @@ export default function Tabuleiro({ xIsNext, squares, onPlay }) {
     </div>
     </>
   );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = "Vai para o movimento " + move;
+    } else {
+      description = "Vai para o início do jogo";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Tabuleiro xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
 }
 
 function calculaVencedor(squares) {
